543dc8fec25db530ce943834bc26c1f9f2064288
WenbinWu2001 <drkwu999@gmail.com>
2026-04-04 00:45:17 -0400

    save before polish

--- ldscore/ldscore_new.py
+++ ldscore/ldscore_new.py
@@ -1,9 +1,13 @@
 '''
-Update LD score calculation code `ldscore.py` to cater (1) R2 table as input (each row being `SNP1 SNP2 R2`), and (2) annotations are passed either in separate files or in one big annotation file (batch processed, `chr4	140087317	140301492	[binary indicator of inclusion in annot1] [binary indicator of inclusion in annot2])
+Update LD score calculation code `ldscore.py` to cater (1) R2 table as input (each row being `SNP1 SNP2 R2`), and (2) annotations are passed either in separate files or in one big annotation file (batch processed, `chr4	140087317	140301492	[binary indicator of inclusion in annot1] [binary indicator of inclusion in annot2]). Annotations are the output of `bed2annot.py` and are in the same format as the .annot files used in the original code, except that they can be batch processed (multiple annotations in one file) and can also be separated into multiple files (e.g. baseline annot vs query annot). 
 
 Inputs:
-- R2 table file (parquet): A table with the following header: "chr, rsID_1, rsID_2, hg38_bp1, hg38_bp2, hg19_bp_1, hg19_bp_2, hg38_Uniq_ID_1, hg38_Uniq_ID_2, hg19_Uniq_ID_1, hg19_Uniq_ID_2, R2, Dprime, +/-corr", where `Uniq_ID`` is in "chr:pos:ref:alt" format.
+- annotation files (allow batch input). [Need you to help decide the format. In original codebase, it is a list of file paths (`--ref-ld-chr` uses comma separated paths and  "baseline." using a glob pattern to specify the baseline annot per chr)]
+- R2 table file (parquet) / reference panel : If provide an R2 table: a table with the following header: "chr, rsID_1, rsID_2, hg38_bp1, hg38_bp2, hg19_bp_1, hg19_bp_2, hg38_Uniq_ID_1, hg38_Uniq_ID_2, hg19_Uniq_ID_1, hg19_Uniq_ID_2, R2, Dprime, +/-corr", where `Uniq_ID`` is in "chr:pos:ref:alt" format.
 - genome build: hg19 or hg38. Used to determine bp position to use in the R2 table. Only useful when identifier mode is `chr_pos`. If identifier mode is `rsID`, then the genome build is not relevant.
+
+Outputs:
+- .M, .M_5_50, ldscores, w_ld for each annotation, including baseline annotations (keep the format as the original code for baseline annot)
 
 Identifier mode:
 Similar to bed2annot, we will have two snp identifier modes: (1) `chr_pos` and (2) rsID. All the matching and intersection will be done based on the selected identfier mode. Read more in bed2annot.py on this.
@@ -32,7 +36,7 @@ Data structure:
 - The chunk R2 matrix is symmetric, sparse, banded with values in [0,1]. each row and column is one SNP on a chromosome. Encode LD R2 matrix in `scipy.sparse.csr_matrix`. `dtype = float32.` 
 - The annotation matrix (columns are all annotations) is tall, skinny, sparse. Each row is a SNP, each column is an annotation. For each annotation column, SNPs are usually binary coded to denote inclusion in that annotation (e.g., pathway). Could also contain continuous values. 0 denotes no contribution. Consider `scipy.sparse.csr_matrix`. `dtype = float32.` 
 
-
+Later, this module will be used together with bed2annot.py in a larger wrapper function of end-to-end partitioned ldsc pipeline. In the pipeline, this script will be followed by a batch-alike partitioned ldsc wrapper function.  So you need to ensure the relevant ports are flexible.
 '''
 
 
